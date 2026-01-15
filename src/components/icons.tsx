5aa72de6b68c44ae6bd855819acb3fde1b31c280
httaX996 <damindakule@gmail.com>
2026-01-15 23:42:32 +0530

    Update logo alt text from 'AniMovie' to 'CK CineMAX'

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import type * as React from 'react';
 export const AniMovieLogo = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
   <img
     src="/logo.png"
-    alt="AniMovie Logo"
+    alt="CK CineMAX Logo"
     {...props}
     className='h-12 w-auto'
   />
